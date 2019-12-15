0982328c5cd566153a0a280c7ac7016dc884e44e
2218675712 <2218675712@qq.com>
2019-12-15 19:53:25 +0800

    2019年12月15日19:53:15 小计模块

--- day02/02-购物车/js/car.js
+++ day02/02-购物车/js/car.js
@@ -11,7 +11,39 @@ $(function () {
             $(".checkall").prop("checked", true);
         } else {
             $(".checkall").prop("checked", false);
-
         }
+    });
+    // 3. 增减商品数量模块 首先声明一个变量，当我们点击+号（increment），就让这个值++，然后赋值给文本框。
+    //加运算
+    $(".increment").click(function () {
+        var n = $(this).siblings(".itxt").val();
+        n++;
+        $(this).siblings(".itxt").val(n);
+        // 3. 计算小计模块 根据文本框的值 乘以 当前商品的价格  就是 商品的小计
+        var p = $(this).parent().parent().siblings(".p-price").text();
+        //根据下标进行截取
+        p = p.substr(1);
+        //保留2位小数
+        var price = (p * n).toFixed(2);
+        //.parents()寻找多个上级,里面可以写元素
+        $(this).parents(".p-num").siblings(".p-sum").text("￥" + price);
+    });
+    //减运算
+    $(".decrement").click(function () {
+        var n = $(this).siblings(".itxt").val();
+        //如果为1,那么不让往下减了
+        if (n == 1) {
+            return false
+        }
+        n--;
+        $(this).siblings(".itxt").val(n);
+        // 3. 计算小计模块 根据文本框的值 乘以 当前商品的价格  就是 商品的小计
+        var p = $(this).parent().parent().siblings(".p-price").text();
+        //根据下标进行截取
+        p = p.substr(1);
+        //保留2位小数
+        var price = (p * n).toFixed(2);
+        //.parents()寻找多个上级,里面可以写元素
+        $(this).parents(".p-num").siblings(".p-sum").text("￥" + price);
     });
 })
