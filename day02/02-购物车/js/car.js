9034da598c9d471382fbd3d4231a6fa849eb208b
2218675712 <2218675712@qq.com>
2019-12-22 23:44:36 +0800

    2019年12月22日23:44:15 购物车完成

--- day02/02-购物车/js/car.js
+++ day02/02-购物车/js/car.js
@@ -4,6 +4,11 @@ $(function () {
     // 事件可以使用change
     $(".checkall").change(function () {
         $(".j-checkbox,.checkall").prop("checked", $(this).prop("checked"));
+        if ($(this).prop("checked")) {
+            $(".cart-item").addClass("check-cart-item");
+        } else {
+            $(".cart-item").removeClass("check-cart-item");
+        }
     });
     // 2. 如果小复选框被选中的个数等于3 就应该把全选按钮选上，否则全选按钮不选。
     $(".j-checkbox").change(function () {
@@ -11,6 +16,11 @@ $(function () {
             $(".checkall").prop("checked", true);
         } else {
             $(".checkall").prop("checked", false);
+        }
+        if ($(this).prop("checked")) {
+            $(this).parents(".cart-item").addClass("check-cart-item");
+        } else {
+            $(this).parents(".cart-item").removeClass("check-cart-item");
         }
     });
     // 3. 增减商品数量模块 首先声明一个变量，当我们点击+号（increment），就让这个值++，然后赋值给文本框。
@@ -73,6 +83,22 @@ $(function () {
         $(".p-sum").each(function (i, ele) {
             money += Number($(ele).text().substr(1));
         });
-        $(".price-sum em").html("￥"+money.toFixed(2));
-    }
+        $(".price-sum em").html("￥" + money.toFixed(2));
+    };
+    // 6. 删除商品模块
+    // (1) 商品后面的删除按钮
+    $(".p-action a").click(function () {
+        $(this).parents(".cart-item").remove();
+        getSum();
+    });
+    // (2) 删除选中的商品
+    $(".remove-batch").click(function () {
+        $(".j-checkbox:checked").parents(".cart-item").remove();
+        getSum();
+    });
+    // (3) 清空购物车 删除全部商品
+    $(".clear-all").click(function () {
+        $(".cart-item").remove();
+        getSum();
+    });
 })
